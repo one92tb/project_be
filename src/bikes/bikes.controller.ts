8882471c5a5fe9d8b935286614f1924ee4298e7c
one92tb <lukasz.fafara@digimonkeys.com>
2023-11-23 23:22:55 +0100

    create Post with request body

--- src/bikes/bikes.controller.ts
+++ src/bikes/bikes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('bikes')
 export class BikesController {
@@ -10,5 +10,10 @@ export class BikesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `return #${id} bike`;
+  }
+
+  @Post()
+  create(@Body('name') body) {
+    return body;
   }
 }
