e281f634d67dca57779e233359bb0710ffb7897a
one92tb <lukasz.fafara@digimonkeys.com>
2023-11-26 17:58:41 +0100

    create user frieldny error message

--- src/bikes/bikes.service.ts
+++ src/bikes/bikes.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Bike } from './entities/bike.entity';
 
 @Injectable()
 export class BikesService {
-  private bikes: Bike[] = [];
+  private bikes: Bike[] = [
+    {
+      id: 1,
+      colors: [],
+      name: 'Merida',
+      type: 'gravel',
+    },
+  ];
 
   findAll() {
     return this.bikes;
   }
 
   findOne(id: string) {
-    return this.bikes.find((bike) => bike.id === +id);
+    const bike = this.bikes.find((bike) => bike.id === +id);
+
+    if (!bike) {
+      // throw new HttpException(`Bike #${id} not found`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException(`Bike #${id} not found`);
+    }
+
+    return bike;
   }
 
   create(createBikeDto: any) {
