9b2ae58e74c3e5ae50ff4747823c42f66079d68e
one92tb <lukasz.fafara@digimonkeys.com>
2023-11-30 23:45:24 +0100

    create bike entity

--- src/bikes/bikes.module.ts
+++ src/bikes/bikes.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BikesController } from './bikes.controller';
 import { BikesService } from './bikes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bike } from './entities/bike.entity';
 
-@Module({ controllers: [BikesController], providers: [BikesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Bike])],
+  controllers: [BikesController],
+  providers: [BikesService],
+})
 export class BikesModule {}
--- src/bikes/entities/bike.entity.ts
+++ src/bikes/entities/bike.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Bike {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   type: string;
+
+  @Column('json', { nullable: true })
   colors: string[];
 }
